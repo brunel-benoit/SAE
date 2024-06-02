7591ab27f089bed5c078b94dc70d69718a1daefc
KarimIsCoding <karim126traore@gmail.com>
2024-06-02 20:53:23 +0200

    Division Exception pour type double

--- PARTIE_2/TestOperation.java
+++ PARTIE_2/TestOperation.java
@@ -8,7 +8,17 @@ public class TestOperation {
         Expression a = new Addition(deux, trois) ;
         Expression d = new Division(s, a) ;
         System.out.println(d + " = " + d.valeur()) ; 
-        
+         try {
+            if (d.valeur() == Double.POSITIVE_INFINITY || d.valeur() == Double.NEGATIVE_INFINITY){ 
+                // On vérifie que le dénominateur n'est pas zéro car pas d'Exception pour les 0 type double
+                System.out.println("Il y a une erreur, on ne peut pas diviser par 0." );
+            }
+        }
+
+        catch (ArithmeticException e) { 
+            System.out.println("Il y a une erreur, on ne peut pas diviser par 0." );
+        }
+       
     }
     
 }
